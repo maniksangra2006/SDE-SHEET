b41cebc7afe2029f52be7ad57fe0048326b63afc
maniksangra2006 <maniksangra2006@gmail.com>
2026-03-03 16:17:16 +0530

    added filename

---
+++ ARRAY 3/pow(x,n).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  double x;
+  cin>>x;
+  int n ;
+  cin>>n;
+  long long  power=n;
+  double ans=1.0;
+  if(power<0) x=1/x;
+  power=-power;
+     while(power>0){
+        if(power%2==1){
+             ans*=x;
+        }
+        x*=x;
+        power/=2;
+     }
+     return ans;
+}
